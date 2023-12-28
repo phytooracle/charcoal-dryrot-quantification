0d7a9ca95cc6b53eff056e46de980672d9db0130
Emmanuel Gonzalez <emmanuelgonzalez@email.arizona.edu>
2023-12-28 15:53:12 -0700

    change

--- model_training/inference.py
+++ model_training/inference.py
@@ -141,11 +141,7 @@ def main():
     if image_file is not None:
         image = imread(image_file)
     else:
-        selected_image = st.session_state.get('selected_image', None)
-        if selected_image is None:
-            selected_image = image_select("Select Image", images)
-            if st.button('Confirm selection'):
-                st.session_state['selected_image'] = selected_image
+        selected_image = image_select("Select Image", images)
         if selected_image is not None:
             image = imread(selected_image)
 
@@ -158,7 +154,6 @@ def main():
             st.image(imread(result_image_path), caption=f'{model_name} Prediction', use_column_width=True)
         else:
             st.write('Classification: CRS positive' if prediction==1 else 'Classification: CRS negative')
-
 
 # --------------------------------------------------
 if __name__ == '__main__':
