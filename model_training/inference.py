afa0355727a3ce3f4d87809f306126076e328a56
Emmanuel Gonzalez <emmanuelgonzalez@email.arizona.edu>
2023-12-28 16:30:36 -0700

    revert

--- model_training/inference.py
+++ model_training/inference.py
@@ -135,16 +135,13 @@ def main():
         # map_location='cpu'#'cuda:0'
     )
 
-    # Create two sections
-    col1, col2 = st.columns(2)
-
-    # Select an image from the library or upload an image in the first section
+    # Select an image from the library or upload an image
     images = glob.glob('images/test_patches/*.png')  # Replace with your actual image paths
-    image_file = col1.file_uploader("Upload Image", type=['png', 'jpg', 'jpeg'])
+    image_file = st.file_uploader("Upload Image", type=['png', 'jpg', 'jpeg'])
     if image_file is not None:
         image = imread(image_file)
     else:
-        selected_image = col1.image_select("Select Image", images, use_container_width=True)
+        selected_image = image_select("Select Image", images, use_container_width=True)
         if selected_image is not None:
             image = imread(selected_image)
 
@@ -152,14 +149,12 @@ def main():
         # Run inference
         prediction = model.predict_single_image(image)
 
-        # Display the results in the second section
         if model_name in ['UNET', 'FCN', 'DeepLabV3']:
             result_image_path = generate_plot(image=image, prediction=prediction, model=model_name)
-            col2.image(imread(result_image_path), caption=f'{model_name} Prediction', use_column_width=True)
+            st.image(imread(result_image_path), caption=f'{model_name} Prediction', use_column_width=True)
         else:
-            col2.image(image)
-            col2.write('Classification: CRS positive' if prediction==1 else 'Classification: CRS negative')
-
+            st.image(image)
+            st.write('Classification: CRS positive' if prediction==1 else 'Classification: CRS negative')
 
 # --------------------------------------------------
 if __name__ == '__main__':
